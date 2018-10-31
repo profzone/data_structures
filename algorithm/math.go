488e5fbc7ff50213692fd4d26a4feae2c3fd58d8
liyiwen <liyiwen@huoyunren.com>
2018-10-31 18:06:09 +0800

    math BigToUint64

--- algorithm/math.go
+++ algorithm/math.go
@@ -1,5 +1,7 @@
 package algorithm
 
+import "math/big"
+
 func IntMax(a int64, b int64) int64 {
 	if a >= b {
 		return a
@@ -30,4 +32,10 @@ func FloatMin(a float64, b float64) float64 {
 	} else {
 		return a
 	}
+}
+
+// BigToUint64 returns the integer casted to a uint64 and returns whether it
+// overflowed in the process.
+func BigToUint64(v *big.Int) (uint64, bool) {
+	return v.Uint64(), v.BitLen() > 64
 }
