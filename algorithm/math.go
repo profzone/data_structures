3261ce190878babab61601d5f78b587fb7caa97c
liyiwen <liyiwen@huoyunren.com>
2018-11-01 10:57:50 +0800

    two's complement number

--- algorithm/math.go
+++ algorithm/math.go
@@ -2,6 +2,15 @@ package algorithm
 
 import "math/big"
 
+var (
+	tt255     = BigPow(2, 255)
+	tt256     = BigPow(2, 256)
+	tt256m1   = new(big.Int).Sub(tt256, big.NewInt(1))
+	tt63      = BigPow(2, 63)
+	MaxBig256 = new(big.Int).Set(tt256m1)
+	MaxBig63  = new(big.Int).Sub(tt63, big.NewInt(1))
+)
+
 func IntMax(a int64, b int64) int64 {
 	if a >= b {
 		return a
@@ -34,8 +43,33 @@ func FloatMin(a float64, b float64) float64 {
 	}
 }
 
+// BigPow returns a ** b as a big integer.
+func BigPow(a, b int64) *big.Int {
+	r := big.NewInt(a)
+	return r.Exp(r, big.NewInt(b), nil)
+}
+
 // BigToUint64 returns the integer casted to a uint64 and returns whether it
 // overflowed in the process.
 func BigToUint64(v *big.Int) (uint64, bool) {
 	return v.Uint64(), v.BitLen() > 64
+}
+
+// U256 encodes as a 256 bit two's complement number. This operation is destructive.
+func U256(x *big.Int) *big.Int {
+	return x.And(x, tt256m1)
+}
+
+// S256 interprets x as a two's complement number.
+// x must not exceed 256 bits (the result is undefined if it does) and is not modified.
+//
+//   S256(0)        = 0
+//   S256(1)        = 1
+//   S256(2**255)   = -2**255
+//   S256(2**256-1) = -1
+func S256(x *big.Int) *big.Int {
+	if x.Cmp(tt255) < 0 {
+		return x
+	}
+	return new(big.Int).Sub(x, tt256)
 }
